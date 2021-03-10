16bd6b1832ab079708adee0cf27323b72d6ed8e8
Miller Steven Gonzalez Vargas <mgonzalez@sigmasolucionesing.com>
2021-03-10 02:35:38 -0500

    function weatherForCity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,16 +14,17 @@ export class AppComponent implements OnInit {
   responseWeather: any;
   responseWeatherCity: WeatherCity = {};
   responseWeatherParis: any;
-  responseWeatherLyon: any;
+  responseWeatherSydney: any;
   responseForecast: Weather = {};
   fecha: any;
   nameDays: string[] = [];
   forecast: any[] = [];
   dayNow: number = 0;
   options: any = {weekday: 'long'};
+  imgContainer: string = "https://www.hiltonhotels.com/assets/img/Hotel%20Images/Hilton/B/BOGCCHH/BOGCCHH_masthead_blueclouds.jpg";
   urlImageBogota?: string = '';
   urlImageParis?: string = '';
-  urlImageLyon?: string = '';
+  urlImageSydney?: string = '';
   urlsCityDays?: string[] = [];
   urlsImage: UrlApp = {
     sun: "https://www.arartur.com.tr/images/content/1284811945.png",
@@ -91,23 +92,23 @@ export class AppComponent implements OnInit {
       error => console.log(error)
     );
 
-    this.weatherService.getWeather('weather', 'lyon').subscribe(
+    this.weatherService.getWeather('weather', 'sydney').subscribe(
       respuesta => {
-        this.responseWeatherLyon = respuesta;
+        this.responseWeatherSydney = respuesta;
         this.responseWeatherCity = respuesta;
 
         this.responseWeatherCity.weather?.forEach(element => {
           if(element.main == "Clouds"){
-            this.urlImageLyon = this.urlsImage.clouds;
+            this.urlImageSydney = this.urlsImage.clouds;
           }
           if(element.main == "Rain"){
-            this.urlImageLyon = this.urlsImage.rain;
+            this.urlImageSydney = this.urlsImage.rain;
           }
           if(element.main == "Clear"){
-            this.urlImageLyon = this.urlsImage.sun;
+            this.urlImageSydney = this.urlsImage.sun;
           }
           if(element.main == "Mist" || element.main == "Fog"){
-            this.urlImageLyon = this.urlsImage.mist;
+            this.urlImageSydney = this.urlsImage.mist;
           }
         });
       },
@@ -116,6 +117,84 @@ export class AppComponent implements OnInit {
 
     this.weatherService.getWeather('forecast', 'bogota').subscribe(
       respuesta => {
+        this.responseForecast = respuesta;
+        this.obtenerFecha();
+        var contador = this.dayNow + 2;
+        
+          this.responseForecast.list?.forEach(element => {
+            
+            const fecha = new Date(element.dt_txt);
+            const day = fecha.getDate();
+
+            if(this.dayNow <= contador && day != this.dayNow){
+              
+              this.forecast.push(element);
+              this.nameDays.push(fecha.toLocaleDateString("es-ES", this.options)
+                );
+                
+                element.weather.forEach((item: { main: any; }) => {
+                  if(item.main == "Clouds"){
+                    this.urlsCityDays?.push(this.urlsImage.clouds);
+                  }
+                  if(item.main == "Rain"){
+                    this.urlsCityDays?.push(this.urlsImage.rain);
+                  }
+                  if(item.main == "Clear"){
+                    this.urlsCityDays?.push(this.urlsImage.sun);
+                  }
+                  if(item.main == "Mist" || item.main == "Mist"){
+                    this.urlsCityDays?.push(this.urlsImage.mist);
+                  }
+                });
+            }
+
+            this.dayNow = day;
+          });
+      },
+      error => console.log(error)
+    );
+  }
+
+  weatherForCity(ciudad:string) {
+
+    if(ciudad == "paris"){
+      this.imgContainer = "https://www.airtecnics.com/media/8625/Eiffel-Tower-in-Paris.jpg";
+    }
+    if(ciudad == "sydney"){
+      this.imgContainer = "https://hotbook.com.mx/wp-content/uploads/2018/11/sydney-portada.jpg";
+    }
+    if(ciudad == "bogota"){
+      this.imgContainer = "https://www.hiltonhotels.com/assets/img/Hotel%20Images/Hilton/B/BOGCCHH/BOGCCHH_masthead_blueclouds.jpg";
+    }
+
+    this.weatherService.getWeather('weather', ciudad).subscribe(
+      respuesta => {
+        this.responseWeather = respuesta;
+        this.responseWeatherCity = respuesta;
+
+        this.responseWeatherCity.weather?.forEach(element => {
+          if(element.main == "Clouds"){
+            this.urlImageBogota = this.urlsImage.clouds;
+          }
+          if(element.main == "Rain"){
+            this.urlImageBogota = this.urlsImage.rain;
+          }
+          if(element.main == "Clear"){
+            this.urlImageBogota = this.urlsImage.sun;
+          }
+          if(element.main == "Mist" || element.main == "Fog"){
+            this.urlImageBogota = this.urlsImage.mist;
+          }
+        });
+      },
+      
+      error => console.log(error)
+    );
+
+    this.weatherService.getWeather('forecast', ciudad).subscribe(
+      respuesta => {
+        this.forecast = [];
+        this.urlsCityDays = [];
         this.responseForecast = respuesta;
         this.obtenerFecha();
         var contador = this.dayNow + 2;
